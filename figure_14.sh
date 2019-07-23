86fd20fb84622730f243e125f71aee9676f6788e
Jonas Martinez <jonas.martinez-bayona@inria.fr>
2019-07-23 11:06:34 +0200

    Add scripts to generate images from figures 14 and 17

---
+++ figure_14.sh
@@ -0,0 +1,9 @@
+./starshaped2d polygons/fig14/diagonal_fourfold_polygon.txt 1000 diagonal_fourfold_polygon.ppm 4 0
+./starshaped2d polygons/fig14/triangular_oneaxisreflection_polygon.txt 1000 triangular_oneaxisreflection_polygon.ppm 4 1
+./starshaped2d polygons/fig14/triangular_sixfold_polygon.txt 1000 triangular_sixfold_polygon.ppm 4 1
+./starshaped2d polygons/fig14/triangular_threeaxisreflection_polygon.txt 1000 triangular_threeaxisreflection_polygon.ppm 4 1
+./starshaped2d polygons/fig14/triangular_threefold_polygon.txt 1000 triangular_threefold_polygon.ppm 4 1
+./starshaped2d polygons/fig14/triangular_twoaxisreflection_polygon.txt 1000 triangular_twoaxisreflection_polygon.ppm 4 1
+
+
+
---
+++ figure_17.sh
@@ -0,0 +1,6 @@
+./starshaped2d polygons/fig17/1.txt 500 fig17_1.ppm 8 1
+./starshaped2d polygons/fig17/2.txt 500 fig17_2.ppm 8 1
+./starshaped2d polygons/fig17/3.txt 500 fig17_3.ppm 8 1
+./starshaped2d polygons/fig17/4.txt 500 fig17_4.ppm 8 1
+./starshaped2d polygons/fig17/5.txt 500 fig17_5.ppm 8 1
+
